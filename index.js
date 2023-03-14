1ad239a85b269aeca395371281f089f1516ba923
Sheilah <sheilahndiema@gmail.com>
2023-03-14 14:28:42 +0300

    add delete and complete functionality

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //selectors
-const todoInput = document.querySelector(".todoInput");
+const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button")
 const todoList = document.querySelector(".todo-list")
-
+const filterTodo = document.querySelector(".filter-todos")
 
 //Event-Listeners
 todoButton.addEventListener("click", addTodo)
+todoList.addEventListener("click", deleteTodo)
 
 function addTodo(e) {
   e.preventDefault();
@@ -13,22 +14,52 @@ function addTodo(e) {
   todoDiv.classList.add("todo")
   //create li
   const newTodo = document.createElement("li");
-  newTodo.innerText = 'hey';
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item")
   todoDiv.appendChild(newTodo);
+  console.log(newTodo)
   //checkmark button
 
   const completedButton = document.createElement("button");
-  completedButton.textContent = "done";
+  completedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
   completedButton.classList.add("complete-btn")
   todoDiv.appendChild(completedButton);
 
   //Delete button
   const trashButton = document.createElement("button");
-  trashButton.textContent = "delete";
-  trashButton.classList.add("complete-btn")
+  trashButton.innerHTML = "<i class=\"fa-solid fa-trash\"></i>"
+  trashButton.classList.add("trash-btn")
   todoDiv.appendChild(trashButton);
 
 
   //Append to list
   todoList.appendChild(todoDiv);
+  //clear todo input value
+
+  todoInput.value = ""
+}
+
+function deleteTodo(e) {
+  const item = e.target;
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall")
+
+    todo.addEventListener("transitionend", function () {
+
+      todo.remove();
+    })
+  }
+
+  //checkmark
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+
+}
+
+function filterTodo() {
+
 }
