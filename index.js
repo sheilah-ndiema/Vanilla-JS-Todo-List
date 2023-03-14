8f832a2a8941341dbaba1bff3566768ca98e3224
Sheilah <sheilahndiema@gmail.com>
2023-03-14 17:28:19 +0300

    add filter functionality

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button")
 const todoList = document.querySelector(".todo-list")
-const filterTodo = document.querySelector(".filter-todos")
+const filterOption = document.querySelector(".filter-todos")
 
 //Event-Listeners
 todoButton.addEventListener("click", addTodo)
 todoList.addEventListener("click", deleteTodo)
+filterOption.addEventListener("click", filterTodo)
 
 function addTodo(e) {
   e.preventDefault();
@@ -60,6 +61,32 @@ function deleteTodo(e) {
 
 }
 
-function filterTodo() {
+function filterTodo(e) {
+
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    console.log(todo)
+    console.log(todos)
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = 'flex';
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = 'flex';
+
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "incomplete":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = 'flex'
+        } else {
+          todo.style.display = "none";
+        }
+    }
+  });
+
 
 }
